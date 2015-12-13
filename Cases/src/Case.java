d29dc1fe682c98387995c10aeaf4b3c52951cd12
Sephirossss <benj.lemaitre@gmail.com>
2015-12-13 23:40:31 +0100

    Tout

--- Cases/src/Case.java
+++ Cases/src/Case.java
@@ -1,18 +1,27 @@
 import java.awt.Color;
 import java.util.HashMap;
 
-public abstract class Case {
+public abstract class Case<E> {
 
-	HashMap<Case,Integer> InCases;
-	HashMap<Case,Integer> OutCases;
-	Color caseColor;
+	private HashMap<Case<E>,Integer> InCases, OutCases;
+	private HashMap<Integer,E> InMsg;
+	private Color caseColor;
 	
-	
-	HashMap<Integer,Object> InMsg;
-	
+	public Color getCaseColor() {
+		return caseColor;
+	}
+
+	protected void setCaseColor(Color caseColor) {
+		this.caseColor = caseColor;
+	}
+
+	protected HashMap<Integer, E> getInMsg() {
+		return InMsg;
+	}
+
 	public abstract void run();
 	
-	public void addMsg(Case c,Object msg){
+	public void addMsg(Case<E> c,E msg){
 		Integer i = InCases.get(c);
 		InMsg.put(i, msg);
 	}
